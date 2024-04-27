cb6452b2f8c68fe7ec86f6f39a96521155d9aebc
Harsh Kumar <cyrixninja@outlook.com>
2024-04-27 18:27:24 +0530

    Initial Commit

---
+++ ai.py
@@ -0,0 +1,76 @@
+from dotenv import load_dotenv
+import os
+
+# Load the .env file
+load_dotenv()
+
+
+def Image_Analysis(image_data):
+    from azure.ai.vision.imageanalysis import ImageAnalysisClient
+    from azure.ai.vision.imageanalysis.models import VisualFeatures
+    from azure.core.credentials import AzureKeyCredential
+
+    try:
+        endpoint = os.getenv('VISION_ENDPOINT')
+        key = os.getenv('VISION_KEY')
+    except KeyError:
+        print("Missing environment variable 'VISION_ENDPOINT' or 'VISION_KEY'")
+        print("Set them before running this sample.")
+        exit()
+
+    # Create an Image Analysis client
+    client = ImageAnalysisClient(
+        endpoint=endpoint,
+        credential=AzureKeyCredential(key)
+    )
+
+
+        
+    result = client.analyze(
+        image_data=image_data,
+        visual_features=[VisualFeatures.CAPTION],
+        language="en", 
+    )
+
+    print("Image analysis results:")
+    # Print caption results to the console
+    print(" Caption:")
+    if result.caption is not None:
+        print(f"   '{result.caption.text}', Confidence {result.caption.confidence:.4f}")
+
+    return result.caption.text
+
+def Suggest_Use(Object):
+    from openai import AzureOpenAI
+    ENDPOINT = os.getenv('OPENAI_ENDPOINT')
+    API_KEY = os.getenv('OPENAI_KEY')
+    API_VERSION = "2024-02-01"
+    MODEL_NAME = "gpt-4"
+    client = AzureOpenAI(
+    azure_endpoint=ENDPOINT,
+    api_key=API_KEY,
+    api_version=API_VERSION,
+    )
+    MESSAGES = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "What can i make with "+Object+" ?"},
+    ]
+    completion = client.chat.completions.create(
+    model=MODEL_NAME,
+    messages=MESSAGES,
+    )
+    print(completion.model_dump_json(indent=2))
+
+
+with open("sample.jpg", "rb") as f:
+    image_data = f.read()
+
+Object = Image_Analysis(image_data)
+Suggest_Use(Object)
+# Tests
+# Load image to analyze into a 'bytes' object
+#with open("sample.jpg", "rb") as f:
+#    image_data = f.read()
+#Image_Analysis(image_data)
+
+
